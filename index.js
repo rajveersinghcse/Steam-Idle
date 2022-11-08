a38488420ce19a1493fddd8a616dc8957a0ec22e
rajveersinghcse <117706744+rajveersinghcse@users.noreply.github.com>
2022-11-08 11:40:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -24,11 +24,3 @@ user.on('loggedOn', () => {
 
 // If you want to Idle Single or multiple games you have to enter list of game id's into line number 20. 
 // For Example : user.gamesPlayed(['Delete me']); --> user.gamesPlayed([730,1172470]);
-
-//To get the Game Id.
-// step 1. Open your browser and enter the game name.
-// step 2. Open its steam link.
-// step 3. Here you see a specifc number in the link. You have to copy that number and paste in the Line number 16
-
-
-// If you want to make your steam profile offline so your friend dont recive you game online notifications. You have to change the 1->7 in line number 15. Here 1 mean online and and 7 mean offline. You can do this thing with your steam also. This method is optional. 
