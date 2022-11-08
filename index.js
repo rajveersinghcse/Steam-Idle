bb7501ca18bc9fdca784b4dcd0afdc4c31fb6905
Rajveer Singh <rajveersinghcse@gmail.com>
2022-11-08 11:20:17 +0530

    new

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ var acc_name = readLine("Please Enter your Steam User Name: ");
 
 var pass = readLine("Please Enter your Steam Password: ");
 
-var g_id = readInt("Please Enter the Game ID: ");
 
 const SteamUser = require('steam-user');
 
@@ -18,7 +17,11 @@ user.logOn(logOnOptions);
 user.on('loggedOn', () => {
 	console.log(logOnOptions.accountName + ' -Successfully logged on');
 	user.setPersona(1);
-	user.gamesPlayed([g_id]);
+	user.gamesPlayed(['Delete me']);
 });
 
 // You have to enter your account name, password and Game Id.
+
+// If you want to Idle Single or multiple games you have to enter list of game id's into line number 20. 
+
+// For Example : user.gamesPlayed(['Delete me']); --> user.gamesPlayed([730,1172470]);
